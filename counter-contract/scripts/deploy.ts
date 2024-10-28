8ca4b977f1b8ae66a9c1511834b4a30d5ee73203
saisunkari19 <saisunkari19@protonmail.com>
2024-10-28 15:39:15 +0530

    adding deploy contract

--- counter-contract/scripts/deploy.ts
+++ counter-contract/scripts/deploy.ts
@@ -10,10 +10,11 @@ const address = "0x5CCDB6FA3192B0664cD53C42102720bbC711f8718cA18c995507ee2Ff32b8
 
 
 
-async function main() {
+async function deploy() {
+
+    // (address, provider).
     const provider = await Provider.create(process.env.LOCAL_NETWORK_URL!)
     const wallet = WalletUnlocked.fromMnemonic(process.env.MNEMONIC!)
-    // (address, provider).
     wallet.connect(provider)
 
     wallet.getBalances().then((data) => {
@@ -33,27 +34,69 @@ async function main() {
     // Contract interactions to read the data
     const counterContract = new CounterContract(contractAddress, wallet)
     const getCounter = await counterContract.functions.counter().get()
-    const valueBefore  = new BN(getCounter.value).toNumber()
+    const valueBefore = new BN(getCounter.value).toNumber()
     console.log(valueBefore)
 
     // Contract interaction to simulate the tx 
-    const {callResult} = await counterContract.functions.increment().simulate()
+    const { callResult } = await counterContract.functions.increment().simulate()
     console.log(callResult.dryRunStatus)
 
     // Contract interaction to sign the tx 
 
-    const { transactionId, waitForResult} = await counterContract.functions.increment().call()
-    const {value :inc} = await waitForResult();
+    const { transactionId, waitForResult } = await counterContract.functions.increment().call()
+    const { value: inc } = await waitForResult();
     console.log(inc, transactionId)
 
 
     // Counter value atter 
     const getCounterAfter = await counterContract.functions.counter().get()
-    const valueAfter  = new BN(getCounterAfter.value).toNumber()
+    const valueAfter = new BN(getCounterAfter.value).toNumber()
     console.log(valueAfter)
 
 }
 
+
+
+async function main() {
+    const provider = await Provider.create(process.env.LOCAL_NETWORK_URL!)
+    const wallet = WalletUnlocked.fromMnemonic(process.env.MNEMONIC!)
+    wallet.connect(provider)
+
+    // Deploy using contract factory 
+    const factory = new CounterContractFactory(wallet)
+
+    // deploy the contract 
+    const { waitForResult, contractId, waitForTransactionId } = await factory.deploy()
+
+    // retrieve tx id 
+    const transactionId = await waitForTransactionId()
+
+    console.log(`Deployment tx id ${transactionId}`)
+
+    // Awaits it's deployment 
+    const { contract, transactionResult } = await waitForResult();
+    // console.log(`Deployment tx result ${JSON.stringify(transactionResult)}`)
+
+    // Execute the call 
+    const { transactionId: incTxId, waitForResult: waitForCallResult } = await contract.functions.increment().call()
+    console.log(`Incremnt tx id: ${incTxId}`)
+    await waitForCallResult
+
+    // // Execute the call 
+    // const { transactionId: incTxId1, waitForResult: waitForCallResult1 } = await contract.functions.increment().call()
+    // console.log(`Incremnt tx id: ${incTxId1}`)
+    // await waitForCallResult1
+
+    // Query the value 
+    const { value } = await contract.functions.counter().get()
+    console.log(new BN(value).toNumber())
+
+
+    // Counter value atter 
+    const getCounterAfter = await contract.functions.counter().get()
+    const valueAfter = new BN(getCounterAfter.value).toNumber()
+    console.log(valueAfter)
+}
 
 main()
     .then(() => process.exit(0))
